4aa877d3344690054b8f18379fc6dd85283e65be
Raymond Zhou <somecrazyidiot@gmail.com>
2013-10-21 16:36:45 -0400

    Get rid of rain stuff. Simplify to super simple design.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-'use strict';
-
-(function( $ ) {
-  $(function() {
-    $( 'body' ).rain({
-      gravity: 0,
-      color: 'rgba(255, 255, 255, 0.5)',
-      count: 1000,
-      lineWidth: 2,
-      velocity: {
-        x: {
-          min: 0,
-          max: -1
-        },
-        y: {
-          min: 0,
-          max: -2
-        }
-      }
-    });
-  });
-}) ( jQuery );
+(function( window, document, undefined ) {
+  'use strict';
+}) ( window, document );
